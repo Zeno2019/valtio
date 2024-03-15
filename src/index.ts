8fd4163c13c2eca6950db8ab11812ee7136bfe98
lenianwain <its9527@outlook.com>
2024-03-15 21:31:10 +0800

    setup learning env

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-export * from './vanilla.ts'
-export * from './react.ts'
+import { proxy } from './vanilla.ts'
+// export * from './react.ts'
+
+const store = proxy({ count: 1 })
+const root = document.querySelector('#app')!
+const countBtn = root.querySelector<HTMLButtonElement>('#count')!
+
+function update() {
+  countBtn.innerText = store.count.toString()
+}
+
+countBtn.onclick = () => {
+  store.count += 1
+  update()
+}
+
+update()
